df2731b1f54faabf218576a3ee4d11d9720099fd
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2021-02-18 23:57:59 +0500

    Update laba 11.3.cpp

--- laba 11.3.cpp
+++ laba 11.3.cpp
@@ -4,16 +4,15 @@ using namespace std;
 struct Stack
 {
 	int data;
-	Stack* next;
-	
+	Stack* prev;
 };
 
 void StackAdd(Stack*& head, int elem)
 {
-	Stack* now_elem = new Stack;
-	now_elem->data = elem;
-	now_elem->next = head;
-	head = now_elem;
+	Stack* new_elem = new Stack;
+	new_elem->data = elem;
+	new_elem->prev = head;
+	head = new_elem;
 }
 
 void StackPrint(Stack*& head)
@@ -22,8 +21,34 @@ void StackPrint(Stack*& head)
 	int i = 1;
 	while (now_ptr != nullptr)
 	{
-		cout << i++ << ". " << now_ptr->data << endl; 
-		now_ptr = now_ptr->next;
+		cout << i++ << ") " << now_ptr->data << endl;
+		now_ptr = now_ptr->prev;
+	}
+}
+
+void StackDelLast(Stack*& head)
+{
+	Stack* now_ptr = head;
+	Stack* help = head;
+	while (now_ptr->prev != nullptr)
+	{
+		help = now_ptr;
+		now_ptr = now_ptr->prev;
+	}
+	if (now_ptr->data % 2 == 0)
+	{
+		delete now_ptr;
+		help->prev = nullptr;
+	}
+}
+
+void StackDel(Stack*& head)
+{
+	while (head != nullptr)
+	{
+		Stack* del_ptr = head;
+		head = head->prev;
+		delete del_ptr;
 	}
 }
 
@@ -32,14 +57,25 @@ int main()
 	int StackLen;
 	Stack* head = nullptr;
 	
+	cout << "enter elems while not the 0 digit: ";
 	int elem;
-	while (cin >> elem)
+	int cnt = 1;
+	cout << endl << cnt++ << ") ";
+	cin >> elem;
+	while (elem != 0)
 	{
+		cout << cnt++ << ") ";
 		if (elem == 0) break;
-		StackAdd(head, elem);
+		else StackAdd(head, elem);
+		cin >> elem;
 	}
 
+	StackDelLast(head);
+	cout << endl;
+
 	StackPrint(head);
+
+	StackDel(head);
 
 
 	return 0;
