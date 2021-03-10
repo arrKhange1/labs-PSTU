dc4e0c87bfb4118e23befd5e525291760dd0db50
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2021-03-10 23:31:29 +0500

    Update многофазная.cpp

--- многофазная.cpp
+++ многофазная.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
-using namespace std;		// 4837 9349 A: 4837 B: 9349     4*2 = 8   8 / 2 files = 4 el-ta v serii max  4/4 = 1 - 1S max
-									// 4837 9349 3747 2332 max poravnu 4*4 = 16 / (2 files) = 8 /4 (seriya) = 2 - 2S
-int main()							// A: 4837 3747 B: 9349 2332 C: 48933723 D: 37494732  
-{
+using namespace std;		// 4837 9349 A: 3478 B: 3499 C: 33447899   4*2 = 8   8 / 2 files = 4 el-ta v serii max  4/4 = 1 - 2S max
+									// 4837 9349 3747 2332 max poravnu 4*4 = 16 / (2 files) = 8 /4 (seriya) = 2 - 4S +1 iter
+int main()							// A: 4837 3747 B: 9349 2332 C: 34892337 D: 34792347 A*: 33447899 ????
+{								// 4837 9349 1289 A: 3478 1289 B: 3499 C:  D: 374989 
 	
 	return 0;
 }
