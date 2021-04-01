1ac207305c1ba1226feef23c32a236e5639a8687
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2021-04-01 20:32:50 +0500

    Update labs 18_classes.h

--- labs 18_classes.h
+++ labs 18_classes.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Triangle
@@ -141,7 +142,17 @@ protected:
 
 ////
 
-// also for 18.7 (templates)
+// also for 18.7 (templates) and 18.9 (exceptions)
+
+class Error // for 18.9 (2 variant of realization)
+{
+public:
+	Error(string exc) { this->exc = exc; }
+	void PrintError() { cout << endl << exc << endl; }
+private:
+	string exc;
+
+};
 
 template<typename T>
 class Set
@@ -153,6 +164,7 @@ public:
 	Set(int);
 	Set(const Set<T>&);
 	T& operator [](int);
+	void Del();
 	Set<T>& operator = (const Set<T>&);
 	int operator ()();
 	Set<T> operator - (const Set<T>&);
@@ -162,6 +174,10 @@ private:
 };
 
 // определение методов ШАБЛОННЫХ классов должно быть в одном хедере с объявлением класса
+#define MAX_SIZE 10
+#define FIRST_VARIANT 1
+#define SECOND_VARIANT 0
+
 template<typename T>
 Set<T>::Set()
 {
@@ -171,6 +187,8 @@ Set<T>::Set()
 template<typename T>
 Set<T>::Set(int size)
 {
+	if (size > MAX_SIZE and FIRST_VARIANT) throw 1;
+	else if (size > MAX_SIZE and SECOND_VARIANT) throw Error("\nSize > MAX_SIZE\n");
 	this->size = size;
 	arr = new T[size];
 	for (int i = 0; i < size; ++i)
@@ -198,14 +216,25 @@ Set<T>::~Set()
 template<typename T>
 T& Set<T>::operator[](int index)
 {
+	if (index >= size and FIRST_VARIANT) throw 2;
+	else if (index < 0 and FIRST_VARIANT) throw 3;
+	if (index >= size and SECOND_VARIANT) throw Error("\nIndex >= Size\n");
+	else if (index < 0 and SECOND_VARIANT) throw Error("\nIndex < 0\n");
 	if (index < size) return arr[index];
-	else cout << "\nIndex is out of range\n";
 }
 template<typename T>
 int Set<T>::operator()()
 {
 	return size;
 }
+template<typename T>
+void Set<T>::Del()
+{
+	if (size == 0 and FIRST_VARIANT) throw 4;
+	else if (size == 0 and SECOND_VARIANT) throw Error("\nNothing to be deleted!\n");
+	--size;
+}
+
 template<class T>
 Set<T>& Set<T>::operator=(const Set<T>& obj)
 {
@@ -221,6 +250,12 @@ Set<T>& Set<T>::operator=(const Set<T>& obj)
 template<typename T>
 Set<T> Set<T>::operator -(const Set<T>& obj)
 {
+	if (this->arr == nullptr and FIRST_VARIANT) throw 6;
+	else if (obj.arr == nullptr and FIRST_VARIANT) throw 7;
+	
+	if (this->arr == nullptr and SECOND_VARIANT) throw Error("\nSets can't be differed > Set1 is empty\n");
+	else if (obj.arr == nullptr and SECOND_VARIANT) throw Error("\nSets can't be differed > Set2 is empty\n");
+	
 	int res;
 	size > obj.size ? res = size : res = obj.size;
 	Set<T> temp(res);
@@ -247,6 +282,7 @@ Set<T> Set<T>::operator -(const Set<T>& obj)
 template <typename T>
 ostream& operator << (ostream& out, const Set<T>& obj)
 {
+
 	if (obj.arr != nullptr)
 	{
 		for (int i = 0; i < obj.size; ++i)
@@ -255,11 +291,13 @@ ostream& operator << (ostream& out, const Set<T>& obj)
 		}
 		cout << endl;
 	}
+	else if (FIRST_VARIANT) throw 8;
+	else if (SECOND_VARIANT) throw Error("\nNothing to be printed!\n");
 	return out;
 }
 
 
-// lab 18.8
+// lab 18.8 
 
 const int evNothing = 0;//пустое событие 
 const int evMessage=100;//непустое событие 
@@ -299,5 +337,9 @@ public:
 protected:
 	int EndState;
 };
+
+// lab 18.9
+
+
 
 //
